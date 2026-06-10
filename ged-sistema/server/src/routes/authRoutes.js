06d6e2c0b6343b222a2f61b6ab7dd029ded361cb
Ygorsantana3 <141068180+Ygorsantana3@users.noreply.github.com>
2026-06-10 14:05:05 -0300

    Delete ged-sistema directory

--- ged-sistema/server/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { login, register } = require('../controllers/authController');
-const { authMiddleware, rbacMiddleware } = require('../middleware/authMiddleware');
-
-router.post('/login', login);
-router.post('/register', authMiddleware, rbacMiddleware('admin'), register);
-
-module.exports = router;
